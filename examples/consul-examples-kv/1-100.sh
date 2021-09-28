03853109cf18687be073f4160a27329cd9a26f7f
Haaike van der Merwe <hvanderm@redhat.com>
2021-09-28 16:59:35 +0200

    added hashicorp reference/url

--- examples/consul-examples-kv/1-100.sh
+++ examples/consul-examples-kv/1-100.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 counter=1
+output=""
+consul_server_ip=54.202.210.81
 while [ $counter -le 100 ]
 do
     fizz=$(( counter % 3 ))
@@ -8,10 +10,14 @@ do
     echo "Counter is $counter"
     if [ "$fizz" -eq 0 ] && [ "$buzz" -eq 0 ]; then
     	echo 'fizzbuzz'
+        consul kv put -http-addr=$consul_server_ip:8500 $counter 'fizzbuzz'
     elif [ "$fizz" -eq 0 ]; then
         echo 'fizz'
+        consul kv put -http-addr=$consul_server_ip:8500 $counter 'fizz'
     elif [ "$buzz" -eq 0 ]; then
         echo 'buzz'
+        consul kv put -http-addr=$consul_server_ip:8500 $counter 'buzz'
     fi
     counter=$(($counter+1))
 done
+
